22d992236e51fc1167d54e6ef97adb394ca1e704
Karmpatel18 <karmpatel1203@gmail.com>
2025-02-06 20:46:33 +0530

    sm c

--- src/components/navbar/Navbar.jsx
+++ src/components/navbar/Navbar.jsx
@@ -9,7 +9,7 @@ function Navbar() {
       
       <div className="flex items-center font-normal tracking-tighter text-neutral-400 dark:text-neutral-200/40 text-sm gap-3">Made by 
       <div className="border-[1px] cursor-pointer p-[2px] w-[34px] h-[34px] rounded-md bg-white border-neutral-200 dark:bg-zinc-900 dark:border-neutral-200/90 transition-all">
-        <img className="rounded-[3px] "src="/navimg.png"/>
+        <a href="https://github.com/Karmpatel18" target="_blank"><img className="rounded-[3px] "src="https://avatars.githubusercontent.com/u/120663033?v=4"/></a>
       </div>
       </div>
       <ThemeMode/>
